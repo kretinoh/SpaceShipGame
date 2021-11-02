371ee5a6ba21be97c7c2ac639b223a61387c4eb5
kretinoh <maestrerafael.r@gmail.com>
2021-11-02 13:19:24 +0100

    Ultima version SpaceShipGame

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ Siguiendo tutorial de https://github.com/techwithtim
 Juego para dos jugadores en local donde manejas tu propia nave e intentas
 eliminar a tu enemigo
 
-TODO  hud
 @author: kretinoh
 '''
 import pygame
 import os
-
 from pygame.constants import K_DOWN, K_LEFT, K_RIGHT, K_UP
-
+pygame.font.init()
 # CONSTANTES
 pygame.display.set_caption("Space Ships!")
 WIDTH, HEIGHT = 900,500
@@ -25,6 +23,9 @@ YELLOW = (255,255,0)
 
 BORDER = pygame.Rect(WIDTH // 2 - 5, 0, 10, HEIGHT)
 
+HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
+WINNER_FONT = pygame.font.SysFont('comicsans', 100)
+
 FPS = 60
 VEL = 3 # Velocidad a la que se moverán las naves
 BULLET_VEL = 7
@@ -35,14 +36,17 @@ YELLOW_HIT = pygame.USEREVENT + 1
 RED_HIT =pygame.USEREVENT + 2
 
 YELLOW_SPACESHIP_IMG = pygame.image.load(
-    os.path.join('Assets', 'spaceship_yellow.png'))
+    os.path.join('Pygame', 'Assets/spaceship_yellow.png'))
 YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
     YELLOW_SPACESHIP_IMG, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
 
 RED_SPACESHIP_IMG = pygame.image.load(
-    os.path.join('Assets', 'spaceship_red.png'))
+    os.path.join('Pygame', 'Assets/spaceship_red.png'))
 RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
     RED_SPACESHIP_IMG, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), -90)
+
+SPACE = pygame.transform.scale(pygame.image.load(
+    os.path.join('Pygame', 'Assets/space.png')), (WIDTH, HEIGHT))
 
 def yellow_handle_movement(keys_pressed, yellow):
         if keys_pressed[pygame.K_a] and yellow.x - VEL > 0: # LEFT
@@ -65,11 +69,18 @@ def red_handle_movement(keys_pressed, red):
             red.y += VEL
 
 
-def draw_window(red, yellow, red_bullets, yellow_bullets):
-    WIN.fill(WHITE)
+def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
+    WIN.blit(SPACE, (0, 0))
     pygame.draw.rect(WIN, BLACK, BORDER)
+
+    red_health_text = HEALTH_FONT.render(f"Health: {red_health}",1,WHITE)
+    yellow_health_text = HEALTH_FONT.render(f"Health: {yellow_health}",1,WHITE)
+    WIN.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10, 10))
+    WIN.blit(yellow_health_text, (10, 10))
+
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
+
 
     for bullet in red_bullets:
         pygame.draw.rect(WIN, RED, bullet)
@@ -84,12 +95,22 @@ def handle_bullets(yellow_bullets, red_bullets, yellow, red):
         if red.colliderect(bullet):
             pygame.event.post(pygame.event.Event(RED_HIT))
             yellow_bullets.remove(bullet)
+        elif bullet.x > WIDTH:
+            yellow_bullets.remove(bullet)
 
     for bullet in red_bullets:
         bullet.x -= BULLET_VEL
         if yellow.colliderect(bullet):
             pygame.event.post(pygame.event.Event(YELLOW_HIT))
             red_bullets.remove(bullet)
+        elif bullet.x < 0:
+            red_bullets.remove(bullet)
+
+def draw_winner(text):
+    draw_text = WINNER_FONT.render(text, 1, WHITE)
+    WIN.blit(draw_text, (WIDTH / 2 - draw_text.get_width()/2, HEIGHT/ 2 - draw_text.get_height()/ 2))
+    pygame.display.update()
+    pygame.time.delay(5000)
 
 # pop-up
 def main():
@@ -99,6 +120,9 @@ def main():
     red_bullets = []
     yellow_bullets = []
 
+    red_health = 10
+    yellow_health = 10
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -107,6 +131,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
@@ -116,6 +141,24 @@ def main():
                 if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
                     bullet = pygame.Rect(red.x, red.y + red.height // 2 - 2, 10, 5)
                     red_bullets.append(bullet)
+        
+            if event.type == RED_HIT:
+                red_health -= 1
+
+            if event.type == YELLOW_HIT:
+                yellow_health -= 1
+
+        winner_text = ""
+        if red_health <= 0:
+            winner_text = "Yellow WINS!!"
+
+        if yellow_health <= 0:
+            winner_text = "RED WINS!!"
+        
+        if winner_text != "":
+            draw_winner(winner_text)
+            break
+
 
         keys_pressed = pygame.key.get_pressed()
         yellow_handle_movement(keys_pressed, yellow)
@@ -123,9 +166,9 @@ def main():
 
         handle_bullets(yellow_bullets, red_bullets, yellow, red)
 
-        draw_window(red, yellow, red_bullets, yellow_bullets)
+        draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health)
 
-    pygame.quit()
+    main()
 
 
 # Con esta linea conseguimos hacer que se ejecute el main solo si se llama a este archivo
